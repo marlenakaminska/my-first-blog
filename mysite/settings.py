37778f4251cba6dadfb9e07360490c2e03d0cded
marlenakaminska <marlena.kaminska@smcebi.edu.pl>
2017-12-09 15:31:06 +0100

    Zmieniony kod HTML na stronie.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'st%s)--6wo4dvi*u84quayg7e#3@-gahb!xml@pxxbm*c4mqhy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['marlenakaminska.pythonanywhere.com']
+ALLOWED_HOSTS = ['marlenakaminska.pythonanywhere.com',
+                 '127.0.0.1']
 
 
 # Application definition
